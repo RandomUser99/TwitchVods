dd72e8f1d336255433a9c40baa2cd9978a4cab05
RandomUser99 <jonhrs@gmail.com>
2020-08-05 13:54:50 +0100

    Tidying

--- TwitchVods.Core/Twitch/VideoResponse.cs
+++ TwitchVods.Core/Twitch/VideoResponse.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace TwitchVods.Core.Twitch
 {
@@ -18,41 +18,20 @@ namespace TwitchVods.Core.Twitch
         [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("user_id")]
-        public string UserId { get; set; }
-
-        [JsonProperty("user_name")]
-        public string UserName { get; set; }
-
         [JsonProperty("title")]
         public string Title { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
 
         [JsonProperty("created_at")]
         public DateTime CreatedAt { get; set; }
 
-        [JsonProperty("published_at")]
-        public DateTime PublishedAt { get; set; }
-
         [JsonProperty("url")]
         public string Url { get; set; }
-
-        [JsonProperty("thumbnail_url")]
-        public string ThumbnailUrl { get; set; }
 
         [JsonProperty("viewable")]
         public string Viewable { get; set; }
 
         [JsonProperty("view_count")]
         public int ViewCount { get; set; }
-
-        [JsonProperty("language")]
-        public string Language { get; set; }
-
-        [JsonProperty("type")]
-        public string Type { get; set; }
 
         [JsonProperty("duration")]
         public string Duration { get; set; }
@@ -63,6 +42,4 @@ namespace TwitchVods.Core.Twitch
         [JsonProperty("cursor")]
         public string Cursor { get; set; }
     }
-
-    
 }
